90fc54049486489afee04d99fb0c644b73aad126
Iffatttt <156417458+Iffatttt@users.noreply.github.com>
2026-04-03 07:18:10 +0500

    added 5.9
    digital twin of phantomx pincher

---
+++ forwardKinematics/task_5.9.m
@@ -0,0 +1,63 @@
+% task 5.9
+% creating a digital twin of the phantomx pincher 
+
+% setting up the robot as a rigid body tree, using column vectors for joint angles
+robot = rigidBodyTree('DataFormat','column');
+
+% link lengths in meters
+L1=0.045;  % base to shoulder height
+L2=0.1035; % upper arm length
+L3=0.1035; % forearm length
+L4=0.11;   % wrist to gripper horn length
+
+%setFixedTransform describes the home geometry so theta is 0
+
+% joint 1 - base rotation
+body1 = rigidBody('body1');          % creating rigid body for link 1
+jnt1 = rigidBodyJoint('jnt1','revolute');  % revolute because it rotates
+setFixedTransform(jnt1, eye(4));     % base frame to joint 1 is identity (no transform needed)
+jnt1.JointAxis = [0 0 1];           % joint 1 rotates about z axis (base spins left/right)
+jnt1.PositionLimits = [-150 150]*pi/180;  % dynamixel ax-12a motor limit is +-150 degrees
+body1.Joint = jnt1;                  % attaching joint to body
+addBody(robot, body1, 'base');       % attaching body1 to the base of the robot
+
+% joint 2 - shoulder
+body2 = rigidBody('body2');          % creating rigid body for link 2
+jnt2 = rigidBodyJoint('jnt2','revolute');  % revolute joint
+T_12 = dh(0, -pi/2, L1, 0);        % dh transform from 1 to 2
+setFixedTransform(jnt2, T_12);      % setting the fixed transform between body1 and body2
+jnt2.JointAxis = [0 0 1];           % rotates about its local z axis
+jnt2.PositionLimits = [-150 150]*pi/180;  % same motor limits
+body2.Joint = jnt2;
+addBody(robot, body2, 'body1');     % attaching body2 to body1
+
+% joint 3 - elbow
+body3 = rigidBody('body3');
+jnt3 = rigidBodyJoint('jnt3','revolute');
+T_23 = dh(L2, 0, 0, 0);            % dh transform from frame 2 to 3
+setFixedTransform(jnt3, T_23);
+jnt3.JointAxis = [0 0 1];
+jnt3.PositionLimits = [-150 150]*pi/180;
+body3.Joint = jnt3;
+addBody(robot, body3, 'body2');     % attaching body3 to body2
+
+% joint 4 - wrist
+body4 = rigidBody('body4');
+jnt4 = rigidBodyJoint('jnt4','revolute');
+T_34 = dh(L3, 0, 0, 0);            % dh transform from frame 3 to 4
+setFixedTransform(jnt4, T_34);
+jnt4.JointAxis = [0 0 1];
+jnt4.PositionLimits = [-150 150]*pi/180;
+body4.Joint = jnt4;
+addBody(robot, body4, 'body3');     % attaching body4 to body3
+
+% end-effector frame at the center of the gripper motor horn
+ee = rigidBody('ee');               % no joint needed, just a fixed frame
+T_ee = dh(L4, 0, 0, 0);            %dh transform from frame 4 to ee
+setFixedTransform(ee.Joint, T_ee); % fixed transform, ee doesnt move relative to body4
+addBody(robot, ee, 'body4');        % attaching end-effector to body4
+
+% printing robot details to verify the chain is correct
+showdetails(robot)
+% visualizing the robot in its zero configuration
+show(robot);
