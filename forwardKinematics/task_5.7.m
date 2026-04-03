a7acd23ef99788d534659b80822bc160483cd902
Iffatttt <156417458+Iffatttt@users.noreply.github.com>
2026-04-03 07:16:51 +0500

    added 5.7
    servo2dh function

---
+++ forwardKinematics/task_5.7.m
@@ -0,0 +1,15 @@
+%task 5.7
+function dhJointAngles = servo2dh(jointAngles)
+    
+    % offset: dh_angle = servo_angle + offset
+    offset = [0, 90, 90, 90];   % in radians 
+    sign  = [1, -1, -1, -1];   % depends on rotation direction
+    dhJointAngles = zeros(1,4); %initializing a zero vector 
+
+    %calculating dh joint angles
+    dhJointAngles(1) = sign(1) .* jointAngles(1) + offset(1);
+    dhJointAngles(2) = sign(2) .* jointAngles(2) + offset(2);
+    dhJointAngles(3) = sign(3) .* jointAngles(3) + offset(3);
+    dhJointAngles(4) = sign(4) .* jointAngles(4) + offset(4);
+
+end
