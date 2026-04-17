bd4dd42fc9a59eaec57c3d163bc81a194c3dca68
Iffatttt <156417458+Iffatttt@users.noreply.github.com>
2026-04-17 10:18:07 +0500

    added dh transform func

--- forwardKinematics/task_5.6.m
+++ forwardKinematics/task_5.6.m
@@ -1,3 +1,9 @@
+function T = dh_transform(a, alpha, d, theta)
+    T = [cos(theta), -sin(theta)*cos(alpha),  sin(theta)*sin(alpha), a*cos(theta);
+         sin(theta),  cos(theta)*cos(alpha), -cos(theta)*sin(alpha), a*sin(theta);
+         0,           sin(alpha),             cos(alpha),            d;
+         0,           0,                      0,                     1];
+end
 %task 5.6
 function [x,y,z,R] = pincherFK(jointAngles)
 
