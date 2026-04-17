9dff388089de8fb00619d5192fe9b9bd273bf07d
Iffatttt <156417458+Iffatttt@users.noreply.github.com>
2026-04-17 09:33:32 +0500

    fixed dh home config

--- forwardKinematics/task_5.9.m
+++ forwardKinematics/task_5.9.m
@@ -1,3 +1,10 @@
+function T = dh(a, alpha, d, theta)
+    T = [cos(theta), -sin(theta)*cos(alpha),  sin(theta)*sin(alpha), a*cos(theta);
+         sin(theta),  cos(theta)*cos(alpha), -cos(theta)*sin(alpha), a*sin(theta);
+         0,           sin(alpha),             cos(alpha),            d;
+         0,           0,                      0,                     1];
+end
+
 % task 5.9
 % creating a digital twin of the phantomx pincher 
 
@@ -24,8 +31,8 @@ addBody(robot, body1, 'base');       % attaching body1 to the base of the robot
 % joint 2 - shoulder
 body2 = rigidBody('body2');          % creating rigid body for link 2
 jnt2 = rigidBodyJoint('jnt2','revolute');  % revolute joint
-T_12 = dh(0, pi/2, L1, 0);        % dh transform from 1 to 2
-setFixedTransform(jnt2, T_12);      % setting the fixed transform between body1 and body2
+T_01 = dh(0, pi/2, L1, 0);        % dh transform from 1 to 2
+setFixedTransform(jnt2, T_01);      % setting the fixed transform between body1 and body2
 jnt2.JointAxis = [0 0 1];           % rotates about its local z axis
 jnt2.PositionLimits = [-150 150]*pi/180;  % same motor limits
 body2.Joint = jnt2;
@@ -34,8 +41,8 @@ addBody(robot, body2, 'body1');     % attaching body2 to body1
 % joint 3 - elbow
 body3 = rigidBody('body3');
 jnt3 = rigidBodyJoint('jnt3','revolute');
-T_23 = dh(L2, 0, 0, 0);            % dh transform from frame 2 to 3
-setFixedTransform(jnt3, T_23);
+T_12 = dh(L2, 0, 0, pi/2);            % dh transform from frame 2 to 3
+setFixedTransform(jnt3, T_12);
 jnt3.JointAxis = [0 0 1];
 jnt3.PositionLimits = [-150 150]*pi/180;
 body3.Joint = jnt3;
@@ -44,8 +51,8 @@ addBody(robot, body3, 'body2');     % attaching body3 to body2
 % joint 4 - wrist
 body4 = rigidBody('body4');
 jnt4 = rigidBodyJoint('jnt4','revolute');
-T_34 = dh(L3, 0, 0, 0);            % dh transform from frame 3 to 4
-setFixedTransform(jnt4, T_34);
+T_23 = dh(L3, 0, 0, 0);            % dh transform from frame 3 to 4
+setFixedTransform(jnt4, T_23);
 jnt4.JointAxis = [0 0 1];
 jnt4.PositionLimits = [-150 150]*pi/180;
 body4.Joint = jnt4;
@@ -53,12 +60,12 @@ addBody(robot, body4, 'body3');     % attaching body4 to body3
 
 % end-effector frame at the center of the gripper motor horn
 ee = rigidBody('ee');               % no joint needed, just a fixed frame
-T_ee = dh(L4, 0, 0, 0);            %dh transform from frame 4 to ee
-setFixedTransform(ee.Joint, T_ee); % fixed transform, ee doesnt move relative to body4
+T_34 = dh(L4, 0, 0, 0);            %dh transform from frame 4 to ee
+setFixedTransform(ee.Joint, T_34); % fixed transform, ee doesnt move relative to body4
 addBody(robot, ee, 'body4');        % attaching end-effector to body4
 
 % printing robot details to verify the chain is correct
 showdetails(robot)
 % visualizing the robot in its zero configuration
-q_home = [0; pi/2; 0; 0]; 
+q_home = [0; 0; 0; 0]; 
 show(robot, q_home);
