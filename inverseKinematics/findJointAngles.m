2f959c728e5ad97e8efb3cc0b91b0e27e0b15bff
faiza-khatri <faizas.khatri@gmail.com>
2026-04-18 16:35:24 +0500

    added dependency of subsequent thetas on theta1

--- inverseKinematics/findJointAngles.m
+++ inverseKinematics/findJointAngles.m
@@ -1,38 +1,52 @@
-function solutions = findJointAngles(x,y,z,phi)
-    d1 = 0.045; 
+function solutions = findJointAngles(x, y, z, phi)
+    d1 = 0.045;
     a2 = 0.1035; a3 = 0.1035; a4 = 0.11;
 
-    r = sqrt(x^2 + y^2);
-    s = z - d1;
-    
-    r_bar = r - a4 * cos(phi);
-    s_bar = s - a4 * sin(phi);
+    t1_base = atan2(y, x);  % in (-pi, pi]
 
-    cos_theta3 = (r_bar^2 + s_bar^2 - a2^2 - a3^2) / (2 * a2 * a3);
-    cos_theta3 = max(-1, min(1, cos_theta3));  % clamp to [-1, 1]
-
-    % theta3 solutions
-    
-    if abs(cos_theta3) > 1
-        solutions = [];
-        disp('No solution exists: target out of reach');
-        return;
+    if abs(x) < 1e-9 && abs(y) < 1e-9
+        theta1_candidates = [0, pi];
+    else
+        t1_other = t1_base + pi;
+        if t1_other > pi
+            t1_other = t1_other - 2*pi;  % wrap back into (-pi, pi]
+        end
+        theta1_candidates = [t1_base, t1_other];
     end
-    theta3_solutions = [acos(cos_theta3), -acos(cos_theta3)];
-    theta1_solutions = [atan2(y, x), -2*pi + atan2(y, x)];
 
     solutions = zeros(4, 4);
     idx = 1;
-    for i = 1:length(theta1_solutions)
-        theta1 = theta1_solutions(i);
-        for j = 1:length(theta3_solutions)
-            theta3 = theta3_solutions(j);
-            theta2_geom = atan2(s_bar, r_bar) - atan2(a3 * sin(theta3), a2 + a3 * cos(theta3));
-            theta2 = theta2_geom - pi/2;  
+
+    for i = 1:length(theta1_candidates)
+        theta1 = theta1_candidates(i);
+
+        r = x*cos(theta1) + y*sin(theta1);
+        s = z - d1;
+
+        r_bar = r - a4 * cos(phi);
+        s_bar = s - a4 * sin(phi);
+
+        cos_theta3 = (r_bar^2 + s_bar^2 - a2^2 - a3^2) / (2 * a2 * a3);
+
+        if abs(cos_theta3) > 1 + 1e-9
+            continue;
+        end
+        cos_theta3 = max(-1, min(1, cos_theta3));
+
+        theta3_candidates = [acos(cos_theta3), -acos(cos_theta3)];
+
+        for j = 1:length(theta3_candidates)
+            theta3 = theta3_candidates(j);
+
+            theta2_geom = atan2(s_bar, r_bar) - atan2(a3*sin(theta3), a2 + a3*cos(theta3));
+            theta2 = theta2_geom - pi/2;
             theta4 = phi - theta2_geom - theta3;
+
             solutions(idx, :) = [theta1, theta2, theta3, theta4];
             idx = idx + 1;
         end
     end
+
+    solutions = solutions(1:idx-1, :);
     solutions = atan2(sin(solutions), cos(solutions));
 end
