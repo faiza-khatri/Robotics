0d6c6b614800819ad031e94f42cf3877328bad73
Faiza Salman Khatri <161750227+faiza-khatri@users.noreply.github.com>
2026-04-24 12:16:11 +0500

    corrected the principal axes to align with each other, increased number of expected blocks

--- perception/pose_estimation.m
+++ perception/pose_estimation.m
@@ -22,11 +22,14 @@ function pointcloud_example()
     
     % Get the intrinsics
     depth_intrinsics = depth_stream.get_intrinsics();
+    % to also get color intrinsics:
+    color_stream = profile.get_stream(realsense.stream.color).as('video_stream_profile');
+    color_intrinsics = color_stream.get_intrinsics();
 
     %% Align the frames and then get the frames
     % Get frames. We discard the first couple to allow
     % the camera time to settle
-    for i = 1:5
+    for i = 1:8
         fs = pipe.wait_for_frames();
     end
     
@@ -41,9 +44,9 @@ function pointcloud_example()
     % Change the argument to realsense.stream.color to align to the color
     % image.
     % align_to_depth = realsense.align(realsense.stream.depth);
-    align_to_depth = realsense.align(realsense.stream.color); % I want it to align to color as my coordiantes [u v] will be from color image
+    align_to_color = realsense.align(realsense.stream.color); % I want it to align to color as my coordiantes [u v] will be from color image
     
-    fs = align_to_depth.process(fs);
+    fs = align_to_color.process(fs);
     
     % Stop streaming
     pipe.stop();
@@ -60,20 +63,35 @@ function pointcloud_example()
 
     %% Create a point cloud using MATLAB library
     % Create a MATLAB intrinsics object
-    intrinsics = cameraIntrinsics([depth_intrinsics.fx,depth_intrinsics.fy],[depth_intrinsics.ppx,depth_intrinsics.ppy],size(depth_frame));
+    % intrinsics = cameraIntrinsics([depth_intrinsics.fx,depth_intrinsics.fy],[depth_intrinsics.ppx,depth_intrinsics.ppy],size(depth_frame));
+    % ptCloud = pcfromdepth(depth_frame,1/depth_scaling,intrinsics,ColorImage=color_frame);
+
+    intrinsics = cameraIntrinsics([color_intrinsics.fx, color_intrinsics.fy], ...
+                                  [color_intrinsics.ppx, color_intrinsics.ppy], ...
+                                  [double(color_intrinsics.height), double(color_intrinsics.width)]);
+    ptCloud = pcfromdepth(depth_frame, 1/depth_scaling, intrinsics, ColorImage=color_frame);
     
-    % Create a point cloud
-    ptCloud = pcfromdepth(depth_frame,1/depth_scaling,intrinsics,ColorImage=color_frame);
     figure; pcshow(ptCloud);
     title('Point Cloud');
     
-    maxDistance = 0.015;   % tune this
-    [planeModel, inlierIdx, outlierIdx] = pcfitplane(ptCloud, maxDistance);
+    maxDistance = 0.008;   % tune this
+    [planeModel, inlierIdx, outlierIdx] = pcfitplane(ptCloud, maxDistance, ...
+        'MaxNumTrials', 2000, ...       % more RANSAC iterations = better plane fit
+        'Confidence', 99.5);
 
     % Remove table
     objectsCloud = select(ptCloud, outlierIdx);
     figure; pcshow(objectsCloud);
     title('Plane removed');
+    % 1. Segment the ground points
+
+    % [groundPtsIdx, nonGroundPtCloud, groundPtCloud] = segmentGroundSMRF(ptCloud);
+    
+    % % 2. Visualize the result
+    % pcshowpair(nonGroundPtCloud, groundPtCloud)
+    % title('Non-Ground (Green) and Ground (Red)')
+
+    
 
     colors = objectsCloud.Color;
     hsvVals = rgb2hsv(im2double(colors));
@@ -89,7 +107,7 @@ function pointcloud_example()
     figure; pcshow(redCloud);
     title('Red blocks extracted');
 
-    minDistance = 0.02;   % tune based on spacing
+    minDistance = 0.02;   % tunable
     [labels, numClusters] = pcsegdist(redCloud, minDistance);
 
     clusterSizes = zeros(numClusters,1);
@@ -104,7 +122,7 @@ function pointcloud_example()
 
     minPoints = 500;   % tune this
 
-    numBlocksExpected = 2;
+    numBlocksExpected = 3;
 
     figure;
     
@@ -119,21 +137,61 @@ function pointcloud_example()
         
         % Remove NaNs
         pts = pts(~any(isnan(pts),2),:);
-        
+
         % ---- Position ----
         position = mean(pts,1);
+        % position(3) = 0.4597;
         
         % ---- Orientation via PCA ----
         ptsCentered = pts - position;
-        
+
         [coeff, ~, ~] = pca(ptsCentered);
+
+        % --- FIX: Constrain Z axis to table normal ---
+        % PCA axis 3 (least variance) should be the surface normal,
+        % but its sign and exact direction are arbitrary. Replace it
+        % with the known table normal instead.
+
+        tableNormal = planeModel.Normal;           % unit normal from RANSAC fit
+        tableNormal = tableNormal / norm(tableNormal);
+
+        % The table normal points toward the camera (positive Z in camera frame).
+        % Ensure it points AWAY from the table (upward toward camera).
+        % The block centroid is above the table, so the vector from any table
+        % inlier point to the centroid should agree with the normal direction.
+        % Any point on the table plane: use the plane equation directly.
+        % For plane ax+by+cz+d=0, a point on it is -d*n (along the normal).
+        params   = planeModel.Parameters;             % [a b c d]
+        tablePt  = -params(4) * params(1:3);          % 1x3 point guaranteed on plane
+
+        toBlock  = position - tablePt;
+        if dot(tableNormal, toBlock) > 0
+            tableNormal = -tableNormal;
+        end
+
+        % Use PCA axes 1 and 2 as the X/Y of the block face,
+        % but re-orthogonalize them against the true Z (table normal).
+        % coeff(:,1) = dominant in-plane axis (long axis of block)
+        % coeff(:,2) = secondary in-plane axis
+        Zaxis = tableNormal';                          % 3x1, forced to table normal
+
+        % Re-orthogonalize X against Z via Gram-Schmidt
+        Xaxis = coeff(:,1) - dot(coeff(:,1), Zaxis) * Zaxis;
+        Xaxis = Xaxis / norm(Xaxis);
+
+        % Y = Z cross X to keep right-handed frame
+        Yaxis = cross(Zaxis, Xaxis);
+        Yaxis = Yaxis / norm(Yaxis);
+
+        R = [Xaxis, Yaxis, Zaxis];   % columns are the frame axes
+
         
-        R = coeff;   % 3x3 rotation matrix
+        % R = coeff;   % 3x3 rotation matrix
 
         % Ensure right-handed coordinate frame
-        if det(R) < 0
-            R(:,3) = -R(:,3);
-        end
+        % if det(R) < 0
+        %     R(:,3) = -R(:,3);
+        % end
         eul = rotm2eul(R);   % [yaw pitch roll] in radians
         eul_deg = rad2deg(eul);
 
